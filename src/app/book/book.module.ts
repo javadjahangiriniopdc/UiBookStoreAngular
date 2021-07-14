e6f85b61b8b777bb4ba002c96b24744502671c7c
drjj007 <javad.jahangiri.niopdc@gmail.com>
2021-07-15 01:36:52 +0430

    create project

---
+++ src/app/book/book.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BookRoutingModule } from './book-routing.module';
+import { BookComponent } from './book.component';
+import { SharedModule } from '../shared/shared.module';
+import { NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap'; // add this line
+
+
+
+@NgModule({
+  declarations: [BookComponent],
+  imports: [
+    SharedModule,
+    BookRoutingModule,
+    NgbDatepickerModule,
+  ]
+})
+export class BookModule { }
