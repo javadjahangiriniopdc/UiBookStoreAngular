460d4c3406f2d6afc7701be906ce88f99006e858
drjj007 <javad.jahangiri.niopdc@gmail.com>
2021-07-15 18:02:12 +0430

    AuthorModule

--- src/app/author/author.module.ts
+++ src/app/author/author.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { AuthorRoutingModule } from './author-routing.module';
 import { AuthorComponent } from './author.component';
+import { SharedModule } from '../shared/shared.module';
+import { NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 
 @NgModule({
   declarations: [AuthorComponent],
-  imports: [
-    CommonModule,
-    AuthorRoutingModule
-  ]
+  imports: [SharedModule,
+    AuthorRoutingModule,
+    NgbDatepickerModule],
 })
 export class AuthorModule { }
